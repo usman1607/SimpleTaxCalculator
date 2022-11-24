57531d68c2a1c1fe5b7b2e2e533bb0463541d240
usman1607 <usman1607@yahoo.com>
2022-11-24 14:17:28 +0100

    add some more test to tax service

--- Test/ServiceTest/TaxServiceTest.cs
+++ Test/ServiceTest/TaxServiceTest.cs
@@ -71,7 +71,63 @@ namespace Test.ServiceTest
             var grossIncome = 360000m;
             var expectedPaye = 3600m; //Note that employees who earn not more than the national minimum wage(currently
                                       //NGN 30,000) are no longer liable to tax or deduction of monthly pay-as-you-earn
-                                      //(PAYE).
+                                      //(PAYE), they will only pay 1% of gross income as tax per anum.
+
+            //Act
+            var paye = _taxService.CalculateTax(grossIncome);
+
+            //Assert
+            Assert.Equal(expectedPaye, paye);
+        }
+
+        [Fact]
+        public void CalculateTaxForEmployeeWithTaxableWithin300K_ShouldReturnCorrectPAYE()
+        {
+            //Arrang
+            var grossIncome = 500000m;
+            var expectedPaye = 11760m; 
+
+            //Act
+            var paye = _taxService.CalculateTax(grossIncome);
+
+            //Assert
+            Assert.Equal(expectedPaye, paye);
+        }
+
+        [Fact]
+        public void CalculateTaxForEmployeeWithTaxableWithin600K_ShouldReturnCorrectPAYE()
+        {
+            //Arrang
+            var grossIncome = 1000000m;
+            var expectedPaye = 46960m;
+
+            //Act
+            var paye = _taxService.CalculateTax(grossIncome);
+
+            //Assert
+            Assert.Equal(expectedPaye, paye);
+        }
+
+        [Fact]
+        public void CalculateTaxForEmployeeWithTaxableWithin1100000_ShouldReturnCorrectPAYE()
+        {
+            //Arrang
+            var grossIncome = 1500000m;
+            var expectedPaye = 99600m;
+
+            //Act
+            var paye = _taxService.CalculateTax(grossIncome);
+
+            //Assert
+            Assert.Equal(expectedPaye, paye);
+        }
+
+        [Fact]
+        public void CalculateTaxForEmployeeWithTaxableWithin1600000_ShouldReturnCorrectPAYE()
+        {
+            //Arrang
+            var grossIncome = 2200000m;
+            var expectedPaye = 189648m;
 
             //Act
             var paye = _taxService.CalculateTax(grossIncome);
